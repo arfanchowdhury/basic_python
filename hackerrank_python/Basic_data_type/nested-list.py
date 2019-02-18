a9567d504985cd5b48f880e99ef307c0dfa49033
arfan_chowdhury <chowdhury.arfan60@gmail.com>
2019-02-18 18:43:39 +0600

    nested list

---
+++ hackerrank_python/Basic_data_type/nested-list.py
@@ -0,0 +1,22 @@
+#problem :: 10 Nested-list
+
+#solve ::
+
+if __name__ == '__main__':
+    
+    N = int(input())
+
+    students = list()
+    for i in range(N):
+        students.append([input(), float(input())])
+
+    scores = list(set([students[x][1] for x in range(N)]))
+ 
+    scores.sort()
+
+    students = [x[0] for x in students if x[1] == scores[1]]
+    students.sort()
+
+    for s in students:
+        print(s)
+
