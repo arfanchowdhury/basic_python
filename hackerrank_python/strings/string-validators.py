1e9c5b5a5b794c3a32ea7a7424141e5a27221c43
arfan_chowdhury <chowdhury.arfan60@gmail.com>
2019-02-18 19:43:58 +0600

    string validators

---
+++ hackerrank_python/strings/string-validators.py
@@ -0,0 +1,14 @@
+
+#problem :: 19 string validators
+
+#solve ::
+
+if __name__ == '__main__':
+    s = input()
+    
+    print(any(map(str.isalnum, s)))
+    print(any(map(str.isalpha, s)))
+    print(any(map(str.isdigit, s)))
+    print(any(map(str.islower, s)))
+    print(any(map(str.isupper, s)))
+
