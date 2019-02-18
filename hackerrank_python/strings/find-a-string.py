cc6f9c2c737994028bb1e751a7cfc8f6670ad799
arfan_chowdhury <chowdhury.arfan60@gmail.com>
2019-02-18 19:45:27 +0600

    no changes

--- hackerrank_python/strings/find-a-string.py
+++ hackerrank_python/strings/find-a-string.py
@@ -3,3 +3,16 @@
 
 #solve ::
 
+def count_substring(string, sub_string):
+    total = 0
+    for i in range(0, len(string) - len(sub_string) + 1):
+        if string[i:i+len(sub_string)] == sub_string:
+            total += 1
+    return total
+
+if __name__ == '__main__':
+    string = input().strip()
+    sub_string = input().strip()
+    
+    count = count_substring(string, sub_string)
+    print(count)
