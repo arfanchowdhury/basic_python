579b4b289349cad38b33e30ad80374df8c09f63f
arfan_chowdhury <chowdhury.arfan60@gmail.com>
2019-02-18 17:02:45 +0600

    write a function

---
+++ hackerrank_python/write-a-function.py
@@ -0,0 +1,13 @@
+#problem :: 6 write-a-function
+
+#solve ::
+
+def is_leap(year):
+    leap = False
+    if(year % 400 == 0):
+        leap = True
+    elif(year % 4 == 0 and year % 100 != 0):
+        leap = True
+    return leap
+
+year = int(input())
