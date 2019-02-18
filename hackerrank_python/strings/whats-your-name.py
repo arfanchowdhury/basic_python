153874e06cc6165f15690f765903bbe6caa48eb2
arfan_chowdhury <chowdhury.arfan60@gmail.com>
2019-02-18 19:26:16 +0600

    whats-your-name

---
+++ hackerrank_python/strings/whats-your-name.py
@@ -0,0 +1,11 @@
+#problem :: 16 whats-your-name
+
+#solve ::
+
+def print_full_name(a, b):
+    print("Hello {} {}! You just delved into python.".format(a,b))
+
+if __name__ == '__main__':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)
